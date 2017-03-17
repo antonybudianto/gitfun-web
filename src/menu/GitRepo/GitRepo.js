14776da6c69a8039036af589d1ffb148b5f6b184
Antony Budianto <antonybudianto@gmail.com>
2017-03-17 22:02:31 +0700

    add repo summary

--- src/menu/GitRepo/GitRepo.js
+++ src/menu/GitRepo/GitRepo.js
@@ -35,16 +35,30 @@ export default class GitRepo extends React.Component {
       return (
         <div>Loading @{this.state.username} repos...</div>
       );
+    } else if (this.state.repos.length === 0) {
+      return (
+        <div>@{this.state.username} have zero repo</div>
+      );
     }
+
+    const repos = this.state.repos
+      .filter(repo => !repo.fork);
+    const totalStars = repos.reduce((acc, cur) => acc + cur['stargazers_count'], 0);
+    const totalForks = repos.reduce((acc, cur) => acc + cur['forks'], 0);
+    const totalOpenIssues = repos.reduce((acc, cur) => acc + cur['open_issues'], 0);
+
     return (
       <div className="row">
+        <h3>Repo summary</h3>
+        <h4 title="stars"><strong>{totalStars}</strong> <i className="fa fa-star"></i></h4>
+        <h4 title="forks"><strong>{totalForks}</strong> <i className="fa fa-code-fork"></i></h4>
+        <h4><strong>{totalOpenIssues}</strong> open issues</h4>
+        <br/>
         {
-          this.state.repos.length === 0 ?
-            <div>@{this.state.username} have zero repo</div> :
-          this.state.repos
+          repos
           .filter(repo => !repo.fork)
           .map(repo =>
-            <div className="col-md-4">
+            <div className="col-md-4" key={repo.id}>
               <GitRepoCard key={repo.id} repo={repo}></GitRepoCard>
             </div>
           )
